8660650dcd8bd5eb46a510ce4cfff87c74bf692f
Gianni Siletto <gianbartolomeo.siletto@regione.piemonte.it>
2024-04-09 16:19:04 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -29,4 +29,4 @@ if notification_type == 'email':
     smtp_password = os.environ['INPUT_SMTP_PASSWORD']
     smtp_from_email = os.environ['INPUT_SMTP_FROM_EMAIL']
     mail_aggiuntive = os.environ['INPUT_MAIL_AGGIUNTIVE']
-    giorni_preavviso = os.environ['INPUT_GIORNI_PREAVVISO']
+    giorni_preavviso = int(os.environ['INPUT_GIORNI_PREAVVISO'])
