67f50d844355573323329ffbcac2980cd2d66bcc
Gianni Siletto <gianbartolomeo.siletto@regione.piemonte.it>
2024-04-09 17:51:47 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -103,7 +103,7 @@ def send_email(from_email: str, to_email: list, subject: str, html_body: str):
     message['To'] = ", ".join(to_email)
     ###################################message['To'] = ", ".join(config.mail_aggiuntive)
     logger.info(f'MAIL A-to_email: {mail_to}')
-    logger.info(f'MESSAGE-TO:  {message['To']}')
+    logger.info(f'MESSAGE-TO:  {to_email}')
     logger.info(f'MAIL AGGIUNTIVE:  {config.mail_aggiuntive}')
     message['Subject'] = subject
 
